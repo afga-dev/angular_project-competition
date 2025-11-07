01013c26a06cfb5ea9eeb71f593707bfcc41a7a0
Ana Guerra <afga.work.contact@gmail.com>
2025-11-06 20:53:12 -0600

    refactor(home-stats): move stats to separate component and refresh design

---
+++ src/app/pages/home-stats.component/home-stats.component.ts
@@ -0,0 +1,58 @@
+import { Component, computed, inject, OnInit, signal } from '@angular/core';
+import { CompetitionService } from '../../services/competition.service';
+import { SummaryInterface } from '../../models/summary.interface';
+import { firstValueFrom } from 'rxjs';
+import { LazyLoadingDirective } from '../../directives/lazy-loading.directive';
+
+@Component({
+  selector: 'app-home-stats',
+  standalone: true,
+  imports: [LazyLoadingDirective],
+  templateUrl: './home-stats.component.html',
+  styleUrl: './home-stats.component.css',
+})
+export class HomeStatsComponent implements OnInit {
+  private competitionService = inject(CompetitionService);
+
+  private _summary = signal<SummaryInterface | null>(null);
+  readonly summary = this._summary.asReadonly();
+
+  readonly _isLoading = signal<boolean>(true);
+  readonly isLoading = this._isLoading.asReadonly();
+
+  readonly stats = computed(() =>
+    [
+      { key: 'totalUsers', title: 'Total users' },
+      { key: 'totalCompetitions', title: 'Total competitions' },
+      { key: 'ongoingCompetitions', title: 'Ongoing competitions' },
+      { key: 'upcomingCompetitions', title: 'Upcoming competitions' },
+    ].map((stat, index) => ({
+      ...stat,
+      value: this.summary()?.[stat.key as keyof SummaryInterface],
+      index,
+    }))
+  );
+
+  ngOnInit(): void {
+    this.loadSummary();
+  }
+
+  private async loadSummary() {
+    try {
+      const summary = await firstValueFrom(
+        this.competitionService.getSummary()
+      );
+      this._summary.set(summary);
+    } catch (err) {
+      this._summary.set({
+        totalUsers: 0,
+        totalCompetitions: 0,
+        ongoingCompetitions: 0,
+        upcomingCompetitions: 0,
+      } as SummaryInterface);
+      // console.error(err);
+    } finally {
+      this._isLoading.set(false);
+    }
+  }
+}
--- src/app/pages/home.component/home.component.ts
+++ src/app/pages/home.component/home.component.ts
@@ -1,53 +1,13 @@
 import { CommonModule } from '@angular/common';
-import { Component, computed, inject, signal } from '@angular/core';
-import { firstValueFrom } from 'rxjs';
-import { CompetitionService } from '../../services/competition.service';
-import { SummaryInterface } from '../../models/summary.interface';
-import { HeaderComponent } from "../header.component/header.component";
+import { Component } from '@angular/core';
+import { HeaderComponent } from '../header.component/header.component';
+import { HomeStatsComponent } from '../home-stats.component/home-stats.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, HeaderComponent],
+  imports: [CommonModule, HeaderComponent, HomeStatsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
-  private competitionService = inject(CompetitionService);
-
-  readonly summary = signal<SummaryInterface | null>(null);
-
-  readonly isLoading = computed(() => this.summary() === null);
-
-  readonly stats = computed(() => [
-    { title: 'Total users', value: this.summary()?.totalUsers },
-    { title: 'Total competitions', value: this.summary()?.totalCompetitions },
-    {
-      title: 'Ongoing competitions',
-      value: this.summary()?.ongoingCompetitions,
-    },
-    {
-      title: 'Completed competitions',
-      value: this.summary()?.completedCompetitions,
-    },
-    {
-      title: 'Upcoming competitions',
-      value: this.summary()?.upcomingCompetitions,
-    },
-  ]);
-
-  constructor() {
-    this.loadSummary();
-  }
-
-  private async loadSummary() {
-    try {
-      const summary = await firstValueFrom(
-        this.competitionService.getSummary()
-      );
-      this.summary.set(summary);
-    } catch (err) {
-      // console.log(err);
-    }
-  }
-}
+export class HomeComponent {}
