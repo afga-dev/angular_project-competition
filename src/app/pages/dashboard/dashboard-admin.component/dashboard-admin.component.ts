9330a900475ec9f31b4344d4b30266eeb82fe142
Ana Guerra <afga.work.contact@gmail.com>
2025-11-20 14:42:45 -0600

    refactor(dashboard-admin): clean component logic and refresh UI

--- src/app/pages/dashboard/dashboard-admin.component/dashboard-admin.component.ts
+++ src/app/pages/dashboard/dashboard-admin.component/dashboard-admin.component.ts
@@ -1,10 +1,13 @@
-import { Component, inject, OnInit, signal } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  inject,
+  OnInit,
+  signal,
+} from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { DateValidator } from '../../../validators/date.validator';
-import {
-  Competition,
-  CompetitionCreate,
-} from '../../../models/competition.interface';
+import { Competition } from '../../../models/competition.interface';
 import { firstValueFrom } from 'rxjs';
 import { CompetitionService } from '../../../services/competition.service';
 import { Dashboard } from '../dashboard.component/dashboard.component';
@@ -16,28 +19,45 @@ import { Dashboard } from '../dashboard.component/dashboard.component';
   templateUrl: './dashboard-admin.component.html',
   styleUrl: './dashboard-admin.component.css',
 })
-export class DashboardAdmin implements OnInit {
+export class DashboardAdmin implements OnInit, AfterViewInit {
+  private readonly DEFAULT_STATUS = 'Ongoing';
+
   private formBuilder = inject(FormBuilder);
+
   private competitionService = inject(CompetitionService);
 
-  readonly dashboardMessage = signal<string | null>(null);
-  readonly dashboardError = signal<string | null>(null);
-  readonly isSubmitting = signal(false);
-  readonly competition = signal<Competition | null>(null);
-  readonly competitions = signal<Competition[]>([]);
-  readonly isDeleteModalVisible = signal(false);
+  private _loadError = signal<string | null>(null);
+  readonly loadError = this._loadError.asReadonly();
 
-  readonly dashboardForm = this.formBuilder.nonNullable.group({
+  readonly _submitMessage = signal<string | null>(null);
+  readonly submitMessage = this._submitMessage.asReadonly();
+
+  readonly _submitError = signal<string | null>(null);
+  readonly submitError = this._submitError.asReadonly();
+
+  readonly isSubmitting = signal(false);
+  readonly competitions = signal<Competition[]>([]);
+  readonly competition = signal<Competition | null>(null);
+  readonly isDeleteModalVisible = signal(false);
+  readonly formVisible = signal(false);
+
+  readonly dashboardAdminForm = this.formBuilder.nonNullable.group({
     competitionId: [0],
-    title: ['', [Validators.required, Validators.minLength(3)]],
-    description: ['', [Validators.required, Validators.minLength(3)]],
+    title: ['', [Validators.required, Validators.maxLength(100)]],
+    description: ['', [Validators.required, Validators.maxLength(500)]],
     startDate: ['', [Validators.required, DateValidator.validDate()]],
     endDate: ['', [Validators.required, DateValidator.validDate()]],
-    status: ['Ongoing'],
+    status: [this.DEFAULT_STATUS],
   });
 
   ngOnInit(): void {
     this.loadCompetitions();
+  }
+
+  ngAfterViewInit(): void {
+    setTimeout(() => {
+      this.formVisible.set(true);
+    });
   }
 
   private async loadCompetitions(): Promise<void> {
@@ -48,119 +68,121 @@ export class DashboardAdmin implements OnInit {
       this.competitions.set(competitions);
     } catch (err) {
       // console.error(err);
+      this._loadError.set("Sorry, the data can't be retrieved");
     }
   }
 
-  async submitDashboardForm(): Promise<void> {
-    if (this.dashboardForm.invalid || this.isSubmitting()) return;
+  private async handleSubmit(
+    request: () => Promise<void>,
+    successMessage: string
+  ): Promise<void> {
+    this.isSubmitting.set(true);
+
+    try {
+      await request();
+
+      this.clear();
+
+      this._submitMessage.set(successMessage);
+    } catch (err) {
+      this._submitError.set('An error occurred');
+    } finally {
+      this.isSubmitting.set(false);
+    }
+  }
+
+  async submitDashboardAdminForm(): Promise<void> {
+    if (this.dashboardAdminForm.invalid || this.isSubmitting()) return;
+
+    const formValue = this.dashboardAdminForm.getRawValue();
 
     if (!this.competition()) {
-      const dashboardData: CompetitionCreate = this.dashboardForm.getRawValue();
-
-      this.setDashboardStatus();
-
-      try {
+      return this.handleSubmit(async () => {
         const newCompetition = await firstValueFrom(
-          this.competitionService.onCreate(dashboardData)
+          this.competitionService.onCreate(formValue)
         );
-        this.competitions.set([...this.competitions(), newCompetition]);
-        this.dashboardMessage.set('Competition successfully created!');
-        this.clearDashboardForm();
-      } catch (err) {
-        this.dashboardError.set('An error occurred.');
-      } finally {
-        this.isSubmitting.set(false);
-      }
-      console.log(dashboardData);
-    } else {
-      const dashboardData: Competition = this.dashboardForm.getRawValue();
-      const id = this.competition()?.competitionId;
 
-      if (!id) return;
-
-      this.setDashboardStatus();
-
-      try {
-        await firstValueFrom(
-          this.competitionService.onUpdate(id, dashboardData)
-        );
-        let updatedCompetition: Competition = {
-          ...this.competition(),
-          ...dashboardData,
-        };
-        this.competitions.set(
-          this.competitions().map((c) =>
-            c.competitionId === id ? updatedCompetition : c
-          )
-        );
-        this.dashboardMessage.set('Competition successfully updated!');
-        this.clearDashboardForm();
-        this.competition.set(null);
-      } catch (ee) {
-        this.dashboardError.set('An error occurred.');
-      } finally {
-        this.isSubmitting.set(false);
-      }
+        this.competitions.update((list) => [...list, newCompetition]);
+      }, 'Successfully created!');
     }
+
+    const id = this.competition()?.competitionId;
+    if (!id) return;
+
+    return this.handleSubmit(async () => {
+      await firstValueFrom(this.competitionService.onUpdate(id, formValue));
+
+      const updated: Competition = {
+        ...this.competition(),
+        ...formValue,
+      };
+
+      this.competitions.set(
+        this.competitions().map((c) => (c.competitionId === id ? updated : c))
+      );
+    }, 'Successfully updated!');
   }
 
-  setDashboardStatus() {
-    this.isSubmitting.set(true);
-    this.dashboardMessage.set(null);
-    this.dashboardError.set(null);
-  }
-
-  clearDashboardForm() {
-    this.dashboardForm.reset({ status: 'Ongoing' });
-    this.dashboardForm.markAsPristine();
-    this.dashboardForm.markAsUntouched();
+  clear() {
+    this.dashboardAdminForm.reset({ status: this.DEFAULT_STATUS });
+    this._submitMessage.set(null);
+    this._submitError.set(null);
     this.competition.set(null);
   }
 
-  hasError(controlName: string, error: string): boolean {
-    const control = this.dashboardForm.get(controlName);
-    return !!(control?.touched && control?.hasError(error));
+  editCompetition(c: Competition) {
+    this.competition.set(c);
+    this.dashboardAdminForm.patchValue(this.mapCompetitionToForm(c));
   }
 
-  editCompetition(competition: Competition) {
-    this.competition.set(competition);
-    this.dashboardForm.patchValue({
-      title: competition.title,
-      description: competition.description,
-      startDate: competition.startDate.split('T')[0],
-      endDate: competition.endDate.split('T')[0],
-      status:
-        competition.status === 'Completed'
-          ? 'Completed'
-          : competition.status === 'Upcoming'
-          ? 'Upcoming'
-          : 'Ongoing',
-    });
+  private mapCompetitionToForm(c: Competition) {
+    return {
+      title: c.title,
+      description: c.description,
+      startDate: c.startDate.split('T')[0],
+      endDate: c.endDate.split('T')[0],
+      status: c.status,
+    };
   }
 
   openDeleteModal(competition: Competition) {
     this.competition.set(competition);
+
     this.isDeleteModalVisible.set(true);
   }
 
   closeDeleteModal() {
     this.competition.set(null);
+
     this.isDeleteModalVisible.set(false);
   }
 
-  async confirmDelete() {
+  async confirmDelete(): Promise<void> {
     const id = this.competition()?.competitionId;
 
     if (!id) return;
 
     try {
       await firstValueFrom(this.competitionService.onDelete(id));
+
       this.competitions.update((list) =>
         list.filter((c) => c.competitionId !== id)
       );
+
       this.closeDeleteModal();
     } catch (err) {
       //console.log(err);
     }
+  }
+
+  onBackdropClick(event: MouseEvent): void {
+    if (event.target === event.currentTarget) {
+      this.closeDeleteModal();
+    }
+  }
+
+  hasError(controlName: string, error: string): boolean {
+    const control = this.dashboardAdminForm.get(controlName);
+    return !!(control?.touched && control?.hasError(error));
   }
 }
